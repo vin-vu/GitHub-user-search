34942df740ba6da2872438f8e56553b8c3eeafc8
vin-vu <vince.hvu@gmail.com>
2021-10-13 17:48:46 -0700

    Created component files

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./Header.js"
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <h4>devfinder</h4>
+      </div>
+    </div>
+  );
+}
+
+export default App;
