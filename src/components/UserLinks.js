86f0fb4d2c5d8ab676bb30f65c06287a68165b5b
vin-vu <vince.hvu@gmail.com>
2021-10-18 16:31:22 -0700

    Created searchbar file, created blog, company, and location icon components

--- src/components/UserLinks.js
+++ src/components/UserLinks.js
@@ -1,22 +1,32 @@
 import './UserLinks.css';
+import TwitterIcon from '../icon-components/TwitterIcon';
 import LocationIcon from '../icon-components/LocationIcon';
+import BlogIcon from '../icon-components/BlogIcon';
+import CompanyIcon from '../icon-components/CompanyIcon';
 
 function UserLinks({location, twitter, blog, company}) {
   return (
     <div className="user-links">
       <div className="location-blog">
-        <p>{location ? location : 'Not available'}</p>
-        <p>{blog ? blog : 'Not available'}</p>
+        <div className="location">
+          <LocationIcon />
+          <p>{location ? location : 'Not available'}</p>
+        </div>
+        <div className="blog">
+          <BlogIcon />
+          <p>{blog ? blog : 'Not available'}</p>
+        </div>
       </div>
       <div className="twitter-company">
         <div className="twitter">
-          <LocationIcon />
+          <TwitterIcon />
           <p>{twitter ? twitter : 'Not available'}</p>
         </div>
-        <p>{company ? company : 'Not available'}</p>
+        <div className="company">
+          <CompanyIcon />
+          <p>{company ? company : 'Not available'}</p>
+        </div>
       </div>
-
-
     </div>
     
   )
