beabacf6d9d0478dc4d8793e5a2c3b49a3bd6ce7
vin-vu <vince.hvu@gmail.com>
2021-10-18 19:48:24 -0700

    Added search icon component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,11 @@
 import "./SearchBar.css";
+import SearchIcon from "../icon-components/SearchIcon";
 
 function SearchBar() {
   return (
     <div className="searchbar-container">
-
-        <input className="search-input" type="text" placeholder="Search GitHub Username..."></input>
-
+      <SearchIcon />
+      <input className="search-input" type="text" placeholder="Search GitHub Username..."></input>
     </div>
   )
 }
