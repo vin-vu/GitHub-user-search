382f03755fa4ee47b707b1396e045ee1ba70cf61
vin-vu <vince.hvu@gmail.com>
2021-10-18 17:22:51 -0700

    Added opacity to nonavailable links

--- src/components/UserLinks.js
+++ src/components/UserLinks.js
@@ -1,35 +1,34 @@
-import './UserLinks.css';
-import TwitterIcon from '../icon-components/TwitterIcon';
-import LocationIcon from '../icon-components/LocationIcon';
-import BlogIcon from '../icon-components/BlogIcon';
-import CompanyIcon from '../icon-components/CompanyIcon';
+import "./UserLinks.css";
+import TwitterIcon from "../icon-components/TwitterIcon";
+import LocationIcon from "../icon-components/LocationIcon";
+import BlogIcon from "../icon-components/BlogIcon";
+import CompanyIcon from "../icon-components/CompanyIcon";
 
-function UserLinks({location, twitter, blog, company}) {
+function UserLinks({ location, twitter, blog, company }) {
   return (
     <div className="user-links">
       <div className="location-blog">
-        <div className="location">
+        <div className="location" style={{ opacity: location ? null : 0.5 }}>
           <LocationIcon />
-          <p>{location ? location : 'Not available'}</p>
+          <p>{location ? location : "Not available"}</p>
         </div>
-        <div className="blog">
+        <div className="blog" style={{ opacity: blog ? null : 0.5 }}>
           <BlogIcon />
-          <p>{blog ? blog : 'Not available'}</p>
+          <p>{blog ? blog : "Not available"}</p>
         </div>
       </div>
       <div className="twitter-company">
-        <div className="twitter">
+        <div className="twitter" style={{ opacity: twitter ? null : 0.5 }}>
           <TwitterIcon />
-          <p>{twitter ? twitter : 'Not available'}</p>
+          <p>{twitter ? twitter : "Not available"}</p>
         </div>
-        <div className="company">
+        <div className="company" style={{ opacity: company ? null : 0.5 }}>
           <CompanyIcon />
-          <p>{company ? company : 'Not available'}</p>
+          <p>{company ? company : "Not available"}</p>
         </div>
       </div>
     </div>
-    
-  )
+  );
 }
 
 export default UserLinks;
